1d57a5b845d32d87616613db3f010a258080c752
Artem Korneenkov <artem.korneenkov@gmail.com>
2015-03-07 18:56:56 +0200

    Updated firmware name (for serial interaction)

--- Marlin/language.h
+++ Marlin/language.h
@@ -57,12 +57,20 @@
 #elif MB(HEPHESTOS)
   #define MACHINE_NAME "HEPHESTOS"
   #define FIRMWARE_URL "http://www.bq.com/gb/downloads-prusa-i3-hephestos.html"
+  #elif MB(MAKERPI)
+    #define MACHINE_NAME "MakerPI"
+    #define FIRMWARE_NAME "Marlin MakerPi (based on Marlin V1)"
+    #define FIRMWARE_URL "https://github.com/TheCluster/Marlin"
 #else // Default firmware set to Mendel
   #define MACHINE_NAME "Mendel"
 #endif
 
 #ifdef CUSTOM_MENDEL_NAME
   #define MACHINE_NAME CUSTOM_MENDEL_NAME
+#endif
+
+#ifndef FIRMWARE_NAME
+    #define FIRMWARE_NAME "Marlin V1; Sprinter/grbl mashup for gen6"
 #endif
 
 #ifndef MACHINE_UUID
@@ -113,7 +121,7 @@
 #define MSG_HEATING_COMPLETE                "Heating done."
 #define MSG_BED_HEATING                     "Bed Heating."
 #define MSG_BED_DONE                        "Bed done."
-#define MSG_M115_REPORT                     "FIRMWARE_NAME:Marlin V1; Sprinter/grbl mashup for gen6 FIRMWARE_URL:" FIRMWARE_URL " PROTOCOL_VERSION:" PROTOCOL_VERSION " MACHINE_TYPE:" MACHINE_NAME " EXTRUDER_COUNT:" STRINGIFY(EXTRUDERS) " UUID:" MACHINE_UUID "\n"
+#define MSG_M115_REPORT                     "FIRMWARE_NAME:" FIRMWARE_NAME " FIRMWARE_URL:" FIRMWARE_URL " PROTOCOL_VERSION:" PROTOCOL_VERSION " MACHINE_TYPE:" MACHINE_NAME " EXTRUDER_COUNT:" STRINGIFY(EXTRUDERS) " UUID:" MACHINE_UUID "\n"
 #define MSG_COUNT_X                         " Count X: "
 #define MSG_ERR_KILLED                      "Printer halted. kill() called!"
 #define MSG_ERR_STOPPED                     "Printer stopped due to errors. Fix the error and use M999 to restart. (Temperature is reset. Set it after restarting)"
